f341f561110014566408de5dbeebd0d967054b12
Студент <Student@College.edu>
2023-04-03 19:49:00 +0300

    first commit

---
+++ Cultery/AuthorizationWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Cultery.Base;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cultery.Pages
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        private cutleryEntities Database;
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+            CaptchaSpawn();
+            try
+            {
+                Database = new cutleryEntities();
+            }
+            catch
+            {
+                MessageBox.Show("lol");
+            };
+            CaptchaTextBox.IsEnabled = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User user = Database.User.SingleOrDefault(u => u.UserPassword == PasswordBox.Text && u.UserLogin == LoginTextBox.Text);
+            if (user != null)
+            {
+                if (CaptchaTextBox.Text == textBox1.Text)
+                {
+                    MessageBox.Show("Victory");
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка капчи");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Defeat");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CaptchaSpawn();
+        }
+        private void CaptchaSpawn()
+        {
+            String allowchar = " ";
+            allowchar = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            allowchar += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z";
+            allowchar += "1,2,3,4,5,6,7,8,9,0";
+            //разделитель
+            char[] a = { ',' };
+            //расщепление массива по разделителю
+            String[] ar = allowchar.Split(a);
+            String pwd = "";
+            string temp = " ";
+            Random r = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                temp = ar[(r.Next(0, ar.Length))];
+                pwd += temp;
+            }
+            textBox1.Text = pwd;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow window = new RegistrationWindow();
+            window.Show();
+            Close();
+        }
+    }
+}
---
+++ Cultery/RegistrationWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Cultery.Base;
+using Cultery.Pages;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cultery
+{
+    /// <summary>
+    /// Interaction logic for RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        private cutleryEntities Database;
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                Database = new cutleryEntities();
+            }
+            catch
+            {
+                MessageBox.Show("lol");
+            };
+            RoleComboBox.ItemsSource = Database.Role.ToList();
+        }
+
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Symbol = new Regex(@"[,.<>`~\|/{})(=_!#$%^&*+]");
+            var Number = new Regex(@"[0-9]+");
+            var Char = new Regex(@"[A-Z]+");
+            var Min = new Regex(@".{8,}");
+
+            if (NameTextBox.Text == "" || LNameTextBox.Text == "" || SNameTextBox.Text == "" || PasswordBox.Text == "" || LoginTextBox.Text == "")
+            {
+                MessageBox.Show("Error");
+            }
+            else
+            {
+                Base.User user = new User();
+                user.UserName = NameTextBox.Text;
+                user.UserSurname = SNameTextBox.Text;
+                user.UserPatronymic = LNameTextBox.Text;
+                user.UserLogin = LoginTextBox.Text;
+                user.UserPassword = PasswordBox.Text;
+                user.UserRole = (int)RoleComboBox.SelectedItem;
+
+                if (Symbol.IsMatch(PasswordBox.Text) && Number.IsMatch(PasswordBox.Text) && Char.IsMatch(PasswordBox.Text) && Min.IsMatch(PasswordBox.Text))
+                {
+                    Database.User.Add(user);
+                    Database.SaveChanges();
+                    MessageBox.Show("Create");
+                }
+                else
+                {
+                    MessageBox.Show("password");
+                }
+
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorizationWindow window = new AuthorizationWindow();
+            window.Show();
+            Close();
+        }
+    }
+}
